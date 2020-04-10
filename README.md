5c9e2614fe793a42c2298b3af11b449cea084c62
itachi00000 <>
2020-04-10 20:22:05 +0800

    0. Trying to create CRUD with React Hooks?? 1. Prepare Basic Setup 2. First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Hooks + CRUD
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+# 
+
+
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// compo
+import App from './App';
+// css
+import './bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
