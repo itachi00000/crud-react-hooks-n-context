457aedc46fc601cf74959ecc0a513b554923071b
itachi00000 <58568276+itachi00000@users.noreply.github.com>
2021-05-21 22:15:12 +0800

    Updates

---
+++ precache-manifest.94aa2403c9b259342ea56ebea56676ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68894644217e116164a5899638ea28ac",
+    "url": "/crud-react-hooks-n-context/index.html"
+  },
+  {
+    "revision": "46bfa7f27b31e6dc2981",
+    "url": "/crud-react-hooks-n-context/static/css/main.de9d4c8d.chunk.css"
+  },
+  {
+    "revision": "2d760dad55528b77e795",
+    "url": "/crud-react-hooks-n-context/static/js/2.90f5a47d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/crud-react-hooks-n-context/static/js/2.90f5a47d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46bfa7f27b31e6dc2981",
+    "url": "/crud-react-hooks-n-context/static/js/main.d7aeb367.chunk.js"
+  },
+  {
+    "revision": "3004881fbb2a7d22e0a2",
+    "url": "/crud-react-hooks-n-context/static/js/runtime-main.a3fb929c.js"
+  }
+]);
