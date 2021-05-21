71c6e548a056cb04d6bd419c4c75014917f4ddb6
itachi00000 <58568276+itachi00000@users.noreply.github.com>
2021-05-21 22:23:43 +0800

    config gh-pages, use HashRouter

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { GlobalProvider } from './context/GlobalState';
 
 // comp
@@ -10,9 +10,9 @@ import './bootstrap.min.css';
 
 ReactDOM.render(
   <GlobalProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </GlobalProvider>,
   document.getElementById('root')
 );
