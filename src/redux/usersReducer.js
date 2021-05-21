1647ea06c3acbb91a8cccd7df2d83397cdb0ffdf
itachi0 <58568276+itachi00000@users.noreply.github.com>
2021-05-21 15:22:54 +0800

    add server-url for dev and prod

--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -8,23 +8,25 @@ import {
 
 // redux state=initState  xxxx
 export function usersReducer(state, action) {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
     case ADD_USER:
-      return { ...state, users: [...state.users, action.payload] };
+      return { ...state, users: [...state.users, payload] };
     case FETCH_USERS:
-      return { ...state, users: action.payload, currentUser: null };
+      return { ...state, users: payload, currentUser: null };
     case SEARCH_USER:
-      return { ...state, searchQuery: action.payload };
+      return { ...state, searchQuery: payload };
     case DELETE_USER:
       return {
         ...state,
-        users: state.users.filter(user => user.id !== action.payload)
+        users: state.users.filter((user) => user.id !== payload)
       };
     case GET_USER:
       return {
         ...state,
         users: [],
-        currentUser: action.payload
+        currentUser: payload
       };
     default:
       return state;
